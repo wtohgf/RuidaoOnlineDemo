15a6a73996ed16489249fe79b137bb3a058d907f
Heguofeng <xxx@qq.com>
2015-06-09 10:37:56 +0800

    睿道在线工程建立

---
+++ RuidaoOnlineDemo/RuidaoOnlineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuidaoOnlineDemo
+//
+//  Created by hegf on 15/6/9.
+//  Copyright (c) 2015年 hegf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuidaoOnlineDemo/RuidaoOnlineDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuidaoOnlineDemo
+//
+//  Created by hegf on 15/6/9.
+//  Copyright (c) 2015年 hegf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
